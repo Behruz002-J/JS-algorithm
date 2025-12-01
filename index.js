872e3700d42963d3e0f2d5d74efbe846e6a6bf4f
Behruz Jumanov <behruzjumanov55@gmail.com>
2025-12-01 15:29:29 +0500

    added new changes

---
+++ index.js
@@ -0,0 +1,86 @@
+// let hello = +prompt("Necha marta chiqishini xoxlaysan ko't")
+
+// let count = 1;
+
+// while (count <= hello ) {
+//     console.log("hellos");
+//     count++;
+// }
+// console.log("The end");
+
+// let count = 0;
+// do {
+//     console.log("salom" +count);
+//     count++;
+// } while (count<=5);
+// //1---
+// let k = 15;
+// let n = 10;
+
+// for( let n = 0; n<= k; n++) {
+//     console.log("k marta" + n);
+    
+// }
+
+//2----
+// let a = 12;
+// let b =22;
+// for(let i =a; i<=b; i++) {
+//     console.log("hello " + i);
+    
+// }
+//3----
+// let k = 10;
+// let n = 2;
+
+// for(let i = k; i>n; i--) {
+//     console.log("salom " +i);
+    
+// }
+//4----
+// let n = 1000;//1kg konfet narxi 1000 so'm
+
+// for(let i = 1; i<=10; i=i+1) {
+//     console.log(i+"kg fonfet narxi = " + n*i);
+    
+// }
+
+//5----
+// let k = 100;//1kg konfet narxi 1000 so'm
+
+// for(let i = 1; i<=1.1; ) {
+//     console.log(i+"kg fonfet narxi = " + k*i);
+    
+// }
+
+//6----
+// let a = 1200;
+// for(let i = 1; i<2; i=i+0.1) {
+//     console.log(i.toFixed(2) + " kg konfet narxi =" +a*i );
+    
+// }
+
+//7
+// let a = 13;
+// let b = 20;
+// let sum = 0
+// for(let i = a; i<=b; i++) {
+//     sum+= i
+    
+// }
+// console.log("yig'indi" ,sum);
+
+//8
+
+//10
+// let n= 7;
+// let F = 0;
+
+// for(let i =1; i<= n; i++) {
+//     console.log(1 + "/" + 1);
+//     F = F + 1/i;
+    
+// }
+// console.log(F.toFixed(2));
+
+//11
